1441d2dd274f884955dfde146ebf610c3ddc7e94
aderusudeer18 <aderu.sudeer@gmail.com>
2026-06-12 18:20:19 +0530

    lates code june changes

---
+++ pages/create_job/sign_up.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright 
+import pytest 
+import allure
+
+
+
+class SignUp:
+    def __init__(self,page):
+        self.page=page
+
+    def open(self):
+        self.page.goto("https://aikam-app-qa-793571778940.asia-south1.run.app/")
+
+    def sign_up(self,signup_email,timeout=2000):
+        with allure.step("Verifying sign up with valid email"):
+            self.page.locator('//a[contains(text(),"Sign up")]').click()
+            sign_up_email=self.page.locator('//input[@name="email"]')
+            sign_up_email.fill(signup_email)
+            sign_up_btn=self.page.locator('//button[@type="submit"]')
+            sign_up_btn.click()
+            self.page.reload()
